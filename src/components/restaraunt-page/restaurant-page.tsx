d3cb11023d2534a1057033b87cec7b62e1bf218d
ahmetvaleevrr <ahmetvaleevrr@oooinex.ru>
2025-10-11 10:46:34 +0500

    Добавил состояние блокирования для кнопки

--- src/components/restaraunt-page/restaurant-page.tsx
+++ src/components/restaraunt-page/restaurant-page.tsx
@@ -9,12 +9,17 @@ export const RestaurantPage = () => {
     ? restaurants.find((item) => item.id === selectedId)
     : restaurants[0];
 
+  const selectedRestaurantName = selectedId
+    ? restaurants.find((item) => item.id === selectedId)
+    : restaurants[0];
+
   return (
     <>
       {restaurants.map((restaurant) => (
         <button
           onClick={() => setSelectedId(restaurant.id)}
           key={restaurant.id}
+          disabled={selectedRestaurant!.id === restaurant.id}
         >
           {restaurant.name}
         </button>
