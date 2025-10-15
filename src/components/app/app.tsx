af711df14c72b2cdd1e9d34c8a5b9be344eb920d
ahmetvaleevrr <ahmetvaleevrr@oooinex.ru>
2025-10-15 12:20:35 +0500

    Добавил прогресс бар для скролла

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,10 +1,17 @@
 import { Layout } from "../layout/layout";
 import { RestaurantPage } from "../restaraunt-page/restaurant-page";
+import ScrollProgressBar from "../scrollProgressBar/scrollProgressBar";
 
 export const App = () => {
   return (
     <Layout>
-      <RestaurantPage />
+      <ScrollProgressBar />
+      <RestaurantPage />      
+      <div>
+         <div style={{ height: '2000px' }}>
+          Контент для прокрутки
+         </div>
+      </div>
     </Layout>
   );
 };
---
+++ src/components/scrollProgressBar/scrollProgressBar.tsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+import '../../assets/styles/scrollProgressBar.css';
+
+const ScrollProgressBar = () => {
+  const [scrollProgress, setScrollProgress] = useState(0);
+
+  const updateProgress = () => {
+    const scrollTop = document.documentElement.scrollTop;
+    const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const progress = (scrollTop / scrollHeight) * 100;
+    
+    setScrollProgress(progress);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', updateProgress);
+    
+    return () => {
+      window.removeEventListener('scroll', updateProgress);
+    };
+  }, []);
+
+  return (
+    <div className="scroll-progress-bar">
+      <div 
+        className="scroll-progress-bar__fill"
+        style={{ width: `${scrollProgress}%` }}
+      />
+    </div>
+  );
+};
+
+export default ScrollProgressBar;
