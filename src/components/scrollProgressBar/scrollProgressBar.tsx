13107344fb2853efb0336a8db60779720c2abbd9
ahmetvaleevrr <ahmetvaleevrr@oooinex.ru>
2025-10-16 14:06:38 +0500

    Перенес в useEffect функцию

--- src/components/scrollProgressBar/scrollProgressBar.tsx
+++ src/components/scrollProgressBar/scrollProgressBar.tsx
@@ -4,17 +4,17 @@ import "../../assets/styles/scrollProgressBar.css";
 const ScrollProgressBar = () => {
   const [scrollProgress, setScrollProgress] = useState(0);
 
-  const updateProgress = () => {
-    const scrollTop = document.documentElement.scrollTop;
-    const scrollHeight =
-      document.documentElement.scrollHeight -
-      document.documentElement.clientHeight;
-    const progress = (scrollTop / scrollHeight) * 100;
-
-    setScrollProgress(progress);
-  };
-
   useEffect(() => {
+    const updateProgress = () => {
+      const scrollTop = document.documentElement.scrollTop;
+      const scrollHeight =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+      const progress = (scrollTop / scrollHeight) * 100;
+
+      setScrollProgress(progress);
+    };
+
     window.addEventListener("scroll", updateProgress);
 
     return () => {
